6a8336ab32649495e0ff42d0e895726c0a908a4f
Mario Neto <netto.mtx@gmail.com>
2019-06-28 11:06:31 -0300

    chore: Altera retorno da rota

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/teste", (req, res) => {
-  return res.json({ message: "Hello Hello Hello" });
+  return res.json({ message: "Hellooo Hellooo Hellooo" });
 });
 
 app.listen(3333);
